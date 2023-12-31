d915cd034eb766df85d46d5c775e0571dc125904
Pete Naveen Pearson <drone4four@gmail.com>
2023-12-31 09:10:23 -0500

    Initialize Node and DLL constructors

---
+++ Doubly-Linked-Lists/script.py
@@ -0,0 +1,11 @@
+class Node:
+    def __init__(self):
+        self.value = value
+        self.next = None
+        self.prev = None
+
+class DoublyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.length = 0
