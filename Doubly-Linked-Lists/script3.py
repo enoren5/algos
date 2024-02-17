74c5c5eb9d9c4af751d605fe0791a2d6e33aa04a
Pete Naveen Pearson <drone4four@gmail.com>
2024-02-17 08:18:43 -0500

    Latest iteration with refactored append name

---
+++ Doubly-Linked-Lists/script3.py
@@ -0,0 +1,50 @@
+class Node:
+    # Create a new node with the value passed to the function
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+        self.prev = None
+
+class DoublyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.length = 0
+
+    def add_right(self, value):
+        # If the head property is null set the head and tail to be the newly created node
+        newNode = Node(value)
+        if self.length == 0:
+            self.head = newNode
+            self.tail = newNode
+        # If not... 
+        else:
+            # Set the next property on the tail to be that node
+            self.tail.next = newNode
+            # Set the previous property on the newly created node to be the tail
+            newNode.prev = self.tail
+            # Set the tail to be the newly created node
+            self.tail = newNode
+        # Increment the length
+        self.length = self.length + 1
+        # Return the Doubly Linked List
+        return self
+    
+    def __str__(self):
+        current = self.head
+        result = []
+        while current != self:
+            result.add_right(current.value)
+            current = current.next
+        return str(result)
+    
+# Instantiation and testing
+DLL_obj = DoublyLinkedList()
+print(DLL_obj)
+DLL_obj.add_right(7)
+DLL_obj.add_right(12)
+DLL_obj.add_right(22)
+print(DLL_obj)
+DLL_obj.add_right(11)
+print(DLL_obj)
+print(DLL_obj)
