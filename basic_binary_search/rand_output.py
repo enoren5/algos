2a91bdcd1d0f988f293d4aa9973c0f281a36af0b
Pete Naveen Pearson <drone4four@gmail.com>
2024-02-22 18:39:44 -0500

    Latest for forum - COMPLETED too!

---
+++ basic_binary_search/rand_output.py
@@ -0,0 +1,11 @@
+import random
+
+# Example usage:
+# Generate a list of 1000 random integers
+arr = [random.randint(100, 10000) for _ in range(400)]
+
+# Sort the list in ascending order
+arr.sort()
+
+# Print the sorted list
+print(arr)
