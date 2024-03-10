eabfc5d6e8dbfc333db9eb549b239fdb33d8e284
Pete Naveen Pearson <drone4four@gmail.com>
2024-03-10 08:59:26 -0400

    Resolved almost on first attempt

--- Stacks-And-Queues/script2.py
+++ Stacks-And-Queues/script2.py
@@ -2,6 +2,7 @@ class Node:
     def __init__(self, value):
         self.value = value
         self.next = None
+        self.last = None
 
         
 class Stack:
@@ -38,16 +39,32 @@ class Stack:
     
     
     def add_to_queue(self, value): 
-        pass
-
-
+        new_node = Node(value)
+        if self.size == 0:
+            self.first = new_node
+            self.last = new_node
+            self.size += 1
+        else: 
+            temp = self.first
+            self.first = new_node
+            self.first.next = temp 
+            self.size += 1
+        return self.size
+    
     def remove_from_queue(self):
-        pass
-
+        if self.size == 0: return None
+        temp = self.first
+        if self.size == 1:
+            self.first = None
+            self.last = None
+        if self.size > 1:
+            self.first = self.last.next
+            self.size = self.size - 1
+        return temp.value
 
     def __str__(self):
         if self.size == 0:
-            return "Empty Stack for intialization"
+            return "Empty structure for intialization"
         current = self.first
         elements = []
         while current:
@@ -68,4 +85,13 @@ SAQ.place_on_top(22)
 print(SAQ)
 print(f'First item removed from bottom: {SAQ.remove_from_bottom()}')
 SAQ.remove_from_bottom()
+print(SAQ)
+print(' = = End of stack experimentation. = = ')
+print(' = = Begin queue experimentation :  = = ')
+print(SAQ)
+SAQ.add_to_queue(' queue item 1 ')
+SAQ.add_to_queue(' queue item 2 ')
+SAQ.add_to_queue(' queue item 3 ')
+print(SAQ)
+print(SAQ.remove_from_queue())
 print(SAQ)
