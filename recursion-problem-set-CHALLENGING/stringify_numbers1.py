74d916960745a51267fc0e736da4d74fb5231448
Pete Naveen Pearson <drone4four@gmail.com>
2024-07-10 14:16:46 -0400

    Initialize all 9 challenging recursion exercises

---
+++ recursion-problem-set-CHALLENGING/stringify_numbers1.py
@@ -0,0 +1,37 @@
+'''
+stringifyNumbers
+Write a function called stringifyNumbers which takes in an object and finds all of the values which are numbers and converts them to strings. Recursion would be a great way to solve this!
+
+The exercise intends for you to create a new object with the numbers converted to strings, and not modify the original. Keep the original object unchanged.
+
+
+/*
+let obj = {
+    num: 1,
+    test: [],
+    data: {
+        val: 4,
+        info: {
+            isRight: true,
+            random: 66
+        }
+    }
+}
+/*
+
+stringifyNumbers(obj)
+
+/*
+{
+    num: "1",
+    test: [],
+    data: {
+        val: "4",
+        info: {
+            isRight: true,
+            random: "66"
+        }
+    }
+}
+*/
+'''
