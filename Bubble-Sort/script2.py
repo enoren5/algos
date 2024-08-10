495d776657eb0991f4269a01ca79a3de4dfe3452
Pete Naveen Pearson <drone4four@gmail.com>
2024-08-10 09:57:14 -0400

    Two first attempts at the algorithm for forum thread

---
+++ Bubble-Sort/script2.py
@@ -0,0 +1,25 @@
+arr = [5,3,4,1,2]
+
+print(f"Original list: {arr}")
+
+def bubble(arr): 
+    for iteration in range(0,len(arr)):
+        for idx1,idx2 in arr[iteration]:
+            if idx1 > idx2:
+                temp = arr[idx1]
+                arr[idx1] = arr[idx2]
+                arr[idx2] = temp
+            else:
+                continue
+    return arr
+
+sorted_list = bubble(arr)
+
+print(f"Final sorted list: {sorted_list}")
+
+
+'''
+            temp = arr[0]
+        if arr[idx1] == arr[idx2]
+            arr[idx2] = temp
+'''
